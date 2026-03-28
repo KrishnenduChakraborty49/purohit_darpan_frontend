c0163b09f3f129a725f2a08798ca11dc0b7baeba
KrishnenduChakraborty49 <chakrabortykrishnandu49@gmail.com>
2026-03-28 09:10:23 +0530

    Diagnostic: Adding logs to trace startup blackout on Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+console.log('PUROHIT DARPAN: React Starting...');
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
